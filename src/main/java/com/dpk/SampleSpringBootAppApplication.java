6431b87592ff4a6280953ea1b5abc7f3ef304aec
deepakmandal705 <deepak.mandal705@gmail.com>
2020-06-19 03:17:33 +0530

    added new welcome controller file (testapi/welcome)

---
+++ src/main/java/com/dpk/SampleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.dpk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dpk/SampleSpringBootAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dpk;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleSpringBootAppApplicationTests {
+
+	public SampleSpringBootAppApplicationTests() {
+		
+	}
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}
