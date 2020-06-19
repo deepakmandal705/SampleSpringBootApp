f7945e0af504a0743e1edc639e3bfad7dd11f8c3
deepakmandal705 <deepak.mandal705@gmail.com>
2020-06-19 22:36:55 +0530

    added batch files to stop and start server

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF "%1"=="start" (
-    ECHO start SampleSpringBootApp
+    ECHO starting SampleSpringBootApp
     start "SampleSpringBootApp" java -jar target/SampleSpringBootApp-0.0.1-SNAPSHOT.jar
 ) ELSE IF "%1"=="stop" (
     ECHO stop SampleSpringBootApp
